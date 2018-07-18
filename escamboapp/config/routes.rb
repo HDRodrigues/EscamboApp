bec670ae779c2c6e884e67e005306c05e49a3de9
Helton <helton.dias.rodrigues@gmail.com>
2018-07-18 18:57:38 -0300

    Adicionado a gem para fazer o gerenciamento de rate

--- escamboapp/config/routes.rb
+++ escamboapp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  post '/rate' => 'rater#create', :as => 'rate'
   get 'backoffice', to: 'backoffice/dashboard#index'
   get 'admin', to: 'backoffice/dashboard#index'
 
